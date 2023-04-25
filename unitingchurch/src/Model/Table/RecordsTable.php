2f678fb4fbded22711721aa760d2a4ca2d586a74
Zhenhao <zzhu0019@student.monash.edu>
2023-04-26 02:17:00 +0800

    validator2 fix error

---
+++ unitingchurch/src/Model/Table/RecordsTable.php
@@ -0,0 +1,102 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Records Model
+ *
+ * @property \App\Model\Table\UsersTable&\Cake\ORM\Association\BelongsTo $Users
+ * @property \App\Model\Table\ClientsTable&\Cake\ORM\Association\BelongsTo $Clients
+ *
+ * @method \App\Model\Entity\Record newEmptyEntity()
+ * @method \App\Model\Entity\Record newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\Record[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Record get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Record findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\Record patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Record[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Record|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Record saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Record[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Record[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Record[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Record[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ */
+class RecordsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('records');
+        $this->setDisplayField('record_id');
+        $this->setPrimaryKey('record_id');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER',
+        ]);
+        $this->belongsTo('Clients', [
+            'foreignKey' => 'client_id',
+            'joinType' => 'INNER',
+        ]);
+
+
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->integer('user_id')
+            ->notEmptyString('user_id');
+
+        $validator
+            ->integer('client_id')
+            ->notEmptyString('client_id');
+
+        $validator
+            ->scalar('record_info')
+            ->requirePresence('record_info', 'create')
+            ->notEmptyString('record_info');
+
+        $validator
+            ->dateTime('record_date_time')
+            ->requirePresence('record_date_time', 'create')
+            ->notEmptyDateTime('record_date_time');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules): RulesChecker
+    {
+        $rules->add($rules->existsIn('user_id', 'Users'), ['errorField' => 'user_id']);
+        $rules->add($rules->existsIn('client_id', 'Clients'), ['errorField' => 'client_id']);
+
+        return $rules;
+    }
+}
---
+++ unitingchurch/tests/Fixture/RecordsFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * RecordsFixture
+ */
+class RecordsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'record_id' => 1,
+                'user_id' => 1,
+                'client_id' => 1,
+                'record_info' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'record_date_time' => '2023-04-25 16:47:06',
+            ],
+        ];
+        parent::init();
+    }
+}
