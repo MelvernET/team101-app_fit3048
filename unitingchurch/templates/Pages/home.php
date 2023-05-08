224ac2b43ef2d8cf99781dc4313a0a3899ba2c17
Zhenhao <zzhu0019@student.monash.edu>
2023-05-08 15:40:10 +0800

    fix bugs

--- unitingchurch/templates/Pages/home.php
+++ unitingchurch/templates/Pages/home.php
@@ -699,6 +699,9 @@ $this->Form->setTemplates($formTemplate);
 
 //load map function
     function loadMapScenario() {
+
+
+
         min.length = 0
         addr.length = 0;
         map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
@@ -875,72 +878,9 @@ $this->Form->setTemplates($formTemplate);
 
 
 
-
-        // Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
-        //     var directionsManager = new Microsoft.Maps.Directions.DirectionsManager(map);
-        //     // Set Route Mode to driving
-        //     directionsManager.setRequestOptions({ routeMode: Microsoft.Maps.Directions.RouteMode.driving });
-        //     var waypoint1 = new Microsoft.Maps.Directions.Waypoint({ address: 'Redmond', location: new Microsoft.Maps.Location(47.67683029174805, -122.1099624633789) });
-        //     var waypoint2 = new Microsoft.Maps.Directions.Waypoint({ address: 'Seattle', location: new Microsoft.Maps.Location(47.59977722167969, -122.33458709716797) });
-        //     directionsManager.addWaypoint(waypoint1);
-        //     directionsManager.addWaypoint(waypoint2);
-        //     // Set the element in which the itinerary will be rendered
-        //     directionsManager.setRenderOptions({ itineraryContainer: document.getElementById('printoutPanel') });
-        //     directionsManager.calculateDirections();
-        // });
-
-
-
-
-
-
-
-
-
-        // Microsoft.Maps.loadModule('Microsoft.Maps.Traffic', function () {
-        //     var manager = new Microsoft.Maps.Traffic.TrafficManager(map);
-        //     manager.show();
-        // });
-
-
-
-
-
-        // Microsoft.Maps.loadModule('Microsoft.Maps.Directions', function () {
-        //     var directionsManager = new Microsoft.Maps.Directions.DirectionsManager(map);
-        //     // Set Route Mode to transit
-        //     directionsManager.setRequestOptions({ routeMode: Microsoft.Maps.Directions.RouteMode.transit });
-        //     var waypoint1 = new Microsoft.Maps.Directions.Waypoint({ address: 'Redmond', location: new Microsoft.Maps.Location(47.67683029174805, -122.1099624633789) });
-        //     var waypoint2 = new Microsoft.Maps.Directions.Waypoint({ address: 'Seattle', location: new Microsoft.Maps.Location(47.59977722167969, -122.33458709716797) });
-        //     directionsManager.addWaypoint(waypoint1);
-        //     directionsManager.addWaypoint(waypoint2);
-        //     // Set the element in which the itinerary will be rendered
-        //     directionsManager.setRenderOptions({ itineraryContainer: document.getElementById('printoutPanel') });
-        //     directionsManager.calculateDirections();
-        // });
     }
 
 
-
-    // function selecte(loca) {
-    //     min.length = 0
-    //     index = null;
-    //     // document.getElementById('printoutPanel').innerHTML = '';
-    //     map.entities.clear();
-    //     var pushpinM = new Microsoft.Maps.Pushpin(map.getCenter(), null);
-    //
-    //
-    //     map.entities.push(pushpinM);
-    //     map.entities.clear();
-    //     loca.forEach( function (item) {
-    //         Microsoft.Maps.loadModule('Microsoft.Maps.SpatialMath', function () {
-    //             min.push(Microsoft.Maps.SpatialMath.getDistanceTo(pushpinM.getLocation(), item.getLocation(), Microsoft.Maps.SpatialMath.DistanceUnits.Miles));
-    //
-    //         })
-    //
-    //     })
-    //
-    // }
 
 
 
